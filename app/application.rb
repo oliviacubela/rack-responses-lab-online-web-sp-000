714f76317828b900787834813d8a8cc35cfb387f
Olivia Cubela <oliviacubela@gmail.com>
2020-09-11 16:08:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
-  
+
   def call(env)
     resp = Rack::Response.new
 
-    greeting_1 = 
-    greeting_2 = 
-    
+    greeting_1 =
+    greeting_2 =
+
   end
 end
 
@@ -13,7 +13,7 @@ end
 #   def call(env)
 #     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
 #   end
-#  
+#
 #   def pretty_response
 #     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
 #   end
@@ -22,27 +22,27 @@ end
 
 
 # class Application
-#  
+#
 #   def call(env)
 #     resp = Rack::Response.new
-#  
+#
 #     num_1 = Kernel.rand(1..20)
 #     num_2 = Kernel.rand(1..20)
 #     num_3 = Kernel.rand(1..20)
-#  
+#
 #     resp.write "#{num_1}\n"
 #     resp.write "#{num_2}\n"
 #     resp.write "#{num_3}\n"
-#  
+#
 #     if num_1==num_2 && num_2==num_3
 #       resp.write "You Win"
 #     else
 #       resp.write "You Lose"
 #     end
-#  
+#
 #     resp.finish
 #   end
-#  
+#
 # end
 
 
@@ -57,6 +57,5 @@ end
 
 # If it's before noon, greet the user with "Good Morning!"
 # If it's after noon, greet the user with "Good Afternoon!"
-# Remember that getting the time may return results in 24 hour time!  
+# Remember that getting the time may return results in 24 hour time!
 # Anytime on or after 12 is the afternoon.
-
